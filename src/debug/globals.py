f71cf50ab87ecb854421c8e43ab61fac6b6a9b08
Stefan Taubert <stefan.taubert@posteo.de>
2021-09-16 08:45:02 +0200

    removed debug

--- src/debug/globals.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path("/home/mi/data/tts-preparation")
-SDP_DIR = Path("/home/mi/data/speech-dataset-preprocessing")
-# BASE_DIR = "/datasets/models/taco2pt_v5"
